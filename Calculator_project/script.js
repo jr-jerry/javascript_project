7cd99b0c485146e1a855bd7ffe8f48708daaba8b
codewithJerry <singhsame798@gmail.com>
2023-12-20 22:49:09 +0530

    Updated code

--- Calculator_project/script.js
+++ Calculator_project/script.js
@@ -1,1 +1,36 @@
-const main=document.querySelector("main");
+const input = document.querySelector("input");
+
+const container = document.querySelector(".calc");
+console.log(container);
+container.addEventListener("click", (event) => {
+    calculate(event.target.textContent);
+    console.log(input.value);
+})
+function calculate(passValue) {
+    if (passValue === "%") {
+        input.value = input.value.toString() + passValue;
+
+    }
+    else if (passValue === "^") {
+        input.value = input.value.toString().slice(0, -1);
+    }
+    else if (passValue === "c") {
+        input.value = "";
+
+    }
+    else if (passValue === "=") {
+        if (input.value.toString().includes("%"))
+         {
+            const func = new Function('return ' + input.value.toString().replace('%', "/100"));
+            input.value = func();
+        }
+        else {
+            const func = new Function('return ' + input.value);
+            input.value = func();
+
+        }
+    }
+    else {
+        input.value = input.value.toString() + passValue;
+    }
+}
